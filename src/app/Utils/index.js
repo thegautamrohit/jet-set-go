f79170927394d950fd45d0ce0a5c44c0122e49a9
thegautamrohit <rgautam1997@gmail.com>
2024-02-28 12:13:09 +0530

    date STructure

--- src/app/Utils/index.js
+++ src/app/Utils/index.js
@@ -71,3 +71,14 @@ export const displayFlights = (
 
   return res;
 };
+
+export const formatDate = (date) => {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  let strTime = hours + ":" + minutes + " " + ampm;
+  return strTime;
+};
--- src/app/components/common/Card/Card.js
+++ src/app/components/common/Card/Card.js
@@ -1,48 +1,9 @@
 import React from "react";
 import styles from "./Card.module.scss";
 import { MdFlight } from "react-icons/md";
+import { formatDate } from "@/app/Utils";
 
 function Card({ data }) {
-  // let data = {
-  //   id: "1",
-  //   fare: 3840,
-  //   displayData: {
-  //     source: {
-  //       airport: {
-  //         cityCode: "DEL",
-  //         cityName: "Delhi",
-  //         terminal: "3",
-  //         airportCode: "DEL",
-  //         airportName: "Indira Gandhi Airport",
-  //         countryCode: "IN",
-  //         countryName: "India",
-  //       },
-  //       depTime: "2023-03-31T06:20",
-  //     },
-  //     airlines: [
-  //       {
-  //         airlineCode: "AB",
-  //         airlineName: "JetSpice",
-  //         flightNumber: "1234",
-  //       },
-  //     ],
-  //     stopInfo: "Non stop",
-  //     destination: {
-  //       airport: {
-  //         cityCode: "BOM",
-  //         cityName: "Mumbai",
-  //         terminal: "2",
-  //         airportCode: "BOM",
-  //         airportName: "Mumbai Airport",
-  //         countryCode: "IN",
-  //         countryName: "India",
-  //       },
-  //       arrTime: "2023-03-31T08:40",
-  //     },
-  //     totalDuration: "2h 20m",
-  //   },
-  // };
-
   return (
     <div className={styles.card__wrapper}>
       <div className={styles.card__top}>
@@ -55,10 +16,7 @@ function Card({ data }) {
       <div className={styles.card__middle}>
         <div className={styles.left}>
           {/* <p>Departure</p> */}
-          <p>
-            {new Date(data?.displayData?.source?.depTime).getHours()}:
-            {new Date(data?.displayData?.source?.depTime).getMinutes()}
-          </p>
+          <p>{formatDate(new Date(data?.displayData?.source?.depTime))}</p>
           <p> {data?.displayData?.source?.airport?.airportName} </p>
           <p> Terminal: {data?.displayData?.source?.airport?.terminal} </p>
           <p>
@@ -82,10 +40,9 @@ function Card({ data }) {
 
         <div className={styles.right}>
           {/* <p>Arrival</p> */}
-          <p>
-            {new Date(data?.displayData?.destination?.arrTime).getHours()}:
-            {new Date(data?.displayData?.destination?.arrTime).getMinutes()}
-          </p>
+
+          <p>{formatDate(new Date(data?.displayData?.destination?.arrTime))}</p>
+
           <p> {data?.displayData?.destination?.airport?.airportName} </p>
           <p> Terminal: {data?.displayData?.destination?.airport?.terminal} </p>
           <p>
